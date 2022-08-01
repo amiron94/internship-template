cf206d896e66a19ba1620d329c4c18836f9e431d
ACCESA\andrei.miron <andreimiron94@yahoo.com>
2022-08-01 16:00:40 +0300

    changes

--- first/first.js
+++ first/first.js
@@ -8,9 +8,10 @@ function addItem() {
     .then((data) => {
       data.products.forEach((product) => {
         var wrapper = document.getElementById("productWrapper");
-        var template = document.querySelector("#productrow");
+        var template = document.querySelector("#cardTemplate");
         var clone = template.content.cloneNode(true);
         clone.querySelector("#value").textContent = product.title;
+        clone.querySelector("#image").setAttribute("src", product.thumbnail);
         clone.querySelector("#button-action").id = `id-${product.id}`;
         wrapper.appendChild(clone);
       });
